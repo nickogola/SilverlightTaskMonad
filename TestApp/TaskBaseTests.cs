6a61270ffab86e81f1df34fc7b45c5e185a9871e
Pavel Gatilov <pgatilov@outlook.com>
2014-01-05 20:01:02 +0700

    Added a test to check behavior of the BCL classes

---
+++ TestApp/TaskBaseTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Silverlight.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestApp
+{
+    [TestClass]
+    [Tag("Task")]
+    [Tag("Base")]
+    public class TaskBaseTests
+    {
+        [TestMethod]
+        public void TaskFactory_ProducesCancelledTaskIfFunctionThrowsTaskCancelledExceptionFromCancellationToken()
+        {
+            var cts = new CancellationTokenSource();
+            cts.Cancel();
+            var cancellationToken = cts.Token;
+            var res = Task.Factory.StartNew(cancellationToken.ThrowIfCancellationRequested, cancellationToken);
+
+            while (!res.IsCanceled && !res.IsCompleted && !res.IsFaulted)
+            {
+                Thread.SpinWait(100);
+            }
+
+            Assert.AreEqual(TaskStatus.Canceled, res.Status);
+        }
+    }
+}
